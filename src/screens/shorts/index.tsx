719955a44eb9b9d14d7bf39badffb3899bddb74c
ogechukwujane <ogeonyejekwejane@gmail.com>
2025-02-07 00:23:16 +0100

    shorts screen design

--- src/screens/shorts/index.tsx
+++ src/screens/shorts/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
+import {styles} from './styles';
 
 export const ShortsScreen = () => {
+  const videos = [
+    { id: '1', uri: 'https://www.example.com/video1.mp4' },
+    { id: '2', uri: 'https://www.example.com/video2.mp4' },
+    { id: '3', uri: 'https://www.example.com/video3.mp4' },
+  ];
+  
   return (
     <View>
-      <Text>Short</Text>
+      <View style={styles.videoContainer}>
+        <View style={styles.absoluteWrap}>
+          <Text style={styles.title}>Secrete Billioniare</Text>
+          <Text style={styles.paragraph} numberOfLines={2}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga fugit
+            repudiandae
+          </Text>
+          <Pressable style={styles.playButton}>
+            <Text style={styles.buttonText}>Watch Now</Text>
+          </Pressable>
+        </View>
+        <View style={styles.iconFlex}>
+          <View style={styles.iconCard}>
+            <Pressable style={styles.icon}></Pressable>
+            <Text>12.5k</Text>
+          </View>
+          <View style={styles.iconCard}>
+            <Pressable style={styles.icon}></Pressable>
+            <Text>124</Text>
+          </View>
+          <View style={styles.iconCard}>
+            <Pressable style={styles.icon}></Pressable>
+            <Text>20</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ src/screens/shorts/styles.tsx
@@ -0,0 +1,66 @@
+import {Dimensions, Platform, StyleSheet} from 'react-native';
+const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 40,
+    backgroundColor: '#000',
+  },
+  videoContainer: {
+    width: screenWidth,
+    height: screenHeight - 60,
+    position: 'relative',
+    backgroundColor: 'blue',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 700,
+    color: '#fff',
+  },
+  paragraph: {
+    fontSize: 14,
+    fontWeight: 500,
+    color: '#ffffff',
+  },
+  playButton: {
+    width: screenWidth / 2,
+    height: 44,
+    borderRadius: 8,
+    backgroundColor: '#F30745',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: 700,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  absoluteWrap: {
+    position: 'absolute',
+    bottom: 20,
+    right: 15,
+    left: 15,
+    gap: 10,
+    flexDirection: 'column',
+    width: screenWidth / 1.5,
+  },
+  iconFlex: {
+    position: 'absolute',
+    bottom: 20,
+    right: 15,
+    gap: 8,
+  },
+  iconCard: {
+    gap: 2,
+    alignItems: 'center',
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+});
